4aacc3f097484276ff7d8ddc696953c8e596e5ba
AIZENSOFT <nosirov150519@gmail.com>
2026-09-17 03:07:36 +0700

    feat: export public SDK

---
+++ src/sensum/__init__.py
@@ -0,0 +1,20 @@
+"""Sensum: continuous perception without continuous LLM inference."""
+
+from .attention import AttentionDecision, AttentionPolicy, ThresholdAttention
+from .models import Modality, SensoryEvent, StateChange
+from .runtime import RuntimeStats, SensumRuntime
+from .world import WorldState
+
+__all__ = [
+    "AttentionDecision",
+    "AttentionPolicy",
+    "Modality",
+    "RuntimeStats",
+    "SensoryEvent",
+    "SensumRuntime",
+    "StateChange",
+    "ThresholdAttention",
+    "WorldState",
+]
+
+__version__ = "0.1.0"
